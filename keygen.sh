e24cc22c0bd9c94c9e7bf0e437e5663b50f88314
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-08-31 08:48:36 +0530

    Update keygen.sh

--- keygen.sh
+++ keygen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define destination directory
-mkdir -p everest
+mkdir -p vendor/everest
 destination_dir="vendor/everest/signing"
 
 # Check if the directory for certificates already exists
 if [ -d ~/.android-certs ]; then
-    read -p "~/.android-certs already exists. Do you want to delete it and proceed? (y/n): " y
+    read -p "~/.android-certs already exists. Do you want to delete it and proceed? (y/n): " choice
     if [ "$choice" != "y" ]; then
         echo "Exiting script."
         exit 1
@@ -18,18 +18,30 @@ fi
 default_subject="/C=PH/ST=Philippines/L=Manila/O=RexC/OU=RexC/CN=Rexc/emailAddress=dtiven13@gmail.com"
 
 # Ask the user if they want to use default values or enter new ones
-read -p "Do you want to use the default subject line: '$default_subject'? (y/n): " y
+read -p "Do you want to use the default subject line: '$default_subject'? (y/n): " use_default
 
 if [ "$use_default" == "y" ]; then
     subject="$default_subject"
-     exit 1
+else
+    # Prompt user for certificate subject information
+    echo "Please enter the following details:"
+    read -p "Country Shortform (C): " C
+    read -p "Country Longform (ST): " ST
+    read -p "Location (L): " L
+    read -p "Organization (O): " O
+    read -p "Organizational Unit (OU): " OU
+    read -p "Common Name (CN): " CN
+    read -p "Email Address (emailAddress): " emailAddress
+
+    # Construct subject string for certificates
+    subject="/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN/emailAddress=$emailAddress"
 fi
 
 # Create directory for certificates
 mkdir -p ~/.android-certs
 
 # Generate keys
-for key_type in releasekey platform shared media networkstack nfc testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot; do
+for key_type in releasekey platform shared media networkstack testkey cyngn-priv-app bluetooth sdk_sandbox verifiedboot nfc; do
     ./development/tools/make_key ~/.android-certs/$key_type "$subject"
 done
 
@@ -38,7 +50,7 @@ mkdir -p "$destination_dir/keys"
 mv ~/.android-certs/* "$destination_dir/keys"
 
 # Create product.mk file
-echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := $destination_dir/keys/releasekey" > "$destination_dir/product.mk"
+echo "PRODUCT_DEFAULT_DEV_CERTIFICATE := $destination_dir/keys/releasekey" > "$destination_dir/keys.mk"
 
 # Set appropriate permissions
 chmod -R 755 "$destination_dir/keys"
